7ff3f1a4448f0c3978ac182c3fb260d551bb0b3d
Pierre UNG <pierre.ung195@gmail.com>
2021-12-07 15:17:58 +0100

    Début API REST

--- src/main.py
+++ src/main.py
@@ -1,26 +1,44 @@
 import cv2
 import grid_recognizer as recognizer
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
 
-reco = recognizer.Grid("grids/hashi5.png", 800)
-circles = reco.detect_circles()
 
-#cv2.imshow("Base", reco.img)
-#cv2.imshow("Without shadows", reco.no_shadow_img)d
-#cv2.imshow("Binnary", reco.binary_img)
-#cv2.imshow("Blurred", reco.blurred_img)
-#cv2.imshow("circles", reco.circles_img)
+# REST API
+app = Flask(__name__)
 
-try:
-    reco.set_circles_coordinates()
-except recognizer.UknwCircleCoordsException as e:
-    print(f"{e.circle} \n Coordinates cannot be determined")
-    exit()
+#Add route
+@app.route('/grid_pic', methods=['POST'])
+def create_structure():
+    photo = request.values['photo'] 
+    print(photo)
+    return 
 
-cv2.imshow("boxes", reco.boxes_img)
-cv2.waitKey(0)       
-
-if(circles): # Circle(s) have been detected
-    for circle in reco.circles:
-        cv2.imshow(f"{circle.position}", circle.image)
-        cv2.waitKey(0)
-
+#
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=50000)
+#
+#reco = recognizer.Grid("grids/hashi0.png", 800)
+#circles = reco.detect_circles()
+#
+##cv2.imshow("Base", reco.img)
+##cv2.imshow("Without shadows", reco.no_shadow_img)d
+##cv2.imshow("Binnary", reco.binary_img)
+##cv2.imshow("Blurred", reco.blurred_img)
+##cv2.imshow("circles", reco.circles_img)
+#
+#try:
+#    reco.set_circles_coordinates()
+#except recognizer.UknwCircleCoordsException as e:
+#    print(f"{e.circle} \n Coordinates cannot be determined")
+#    exit()
+#
+#cv2.imshow("boxes", reco.boxes_img)
+#cv2.waitKey(0)       
+#
+#if(circles): # Circle(s) have been detected
+#    for circle in reco.circles:
+#        cv2.imshow(f"{circle.position}", circle.image)
+#        cv2.waitKey(0)
+#
+#
